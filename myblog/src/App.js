d0674f4b57e95b4797e23264b3ef944fd13cd96f
jeremy <poohc300@gmail.com>
2022-07-11 11:53:38 +0900

    feature: 라우팅 적용

--- myblog/src/App.js
+++ myblog/src/App.js
@@ -1,9 +1,16 @@
-import Layout from './components/layout/Layout';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+import Project from './pages/project/Project';
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/project' element={<Project />} />
+      </Routes>
     </div>
   );
 }
---
+++ myblog/src/pages/project/Project.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Project extends React.Component {
+    render() {
+        return (
+            
+            <h1>Project</h1>
+        )
+    }
+}
+
+export default Project;
